70f2116eb3efab24ae987a1b3d713a76ec436bd5
Sagit25 <sukhuny@snu.ac.kr>
2023-02-12 10:11:02 +0900

    Updates

---
+++ precache-manifest.4c7834d8440294ca28d5c6edc1c3cc75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0f4c9d00da33ee58558",
+    "url": "/static/css/main.4944ba65.chunk.css"
+  },
+  {
+    "revision": "f0f4c9d00da33ee58558",
+    "url": "/static/js/main.f0f4c9d0.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "c21e058445d797f0bb26",
+    "url": "/static/js/2.c21e0584.chunk.js"
+  },
+  {
+    "revision": "b3d155d33fc8f3126447",
+    "url": "/static/js/runtime~main.b3d155d3.js"
+  },
+  {
+    "revision": "4d2e387b736d21aee0ba753bad2546b9",
+    "url": "/index.html"
+  }
+];
